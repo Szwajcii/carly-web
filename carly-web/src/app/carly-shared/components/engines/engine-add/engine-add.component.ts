f2caa893f822399df974e7ac3ecac4089c9c2a38
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-18 20:03:28 +0200

    Init engines component structure.

---
+++ carly-web/src/app/carly-shared/components/engines/engine-add/engine-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-engine-add',
+  templateUrl: './engine-add.component.html',
+  styleUrls: ['./engine-add.component.scss']
+})
+export class EngineAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
