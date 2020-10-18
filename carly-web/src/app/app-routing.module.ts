a58ce464be40dea8d3c8aff22be8b710c047caca
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-18 20:01:00 +0200

    Init cars component.

--- carly-web/src/app/app-routing.module.ts
+++ carly-web/src/app/app-routing.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './carly-shared/components/home/home.component';
+import {PartsComponent} from './carly-shared/components/parts/parts.component';
+import {CarsComponent} from './carly-shared/components/cars/cars.component';
+import {OrdersComponent} from './carly-shared/components/orders/orders.component';
+import {EnginesComponent} from './carly-shared/components/engines/engines.component';
+import {EngineAddComponent} from './carly-shared/components/engines/engine-add/engine-add.component';
+import {EngineEditComponent} from './carly-shared/components/engines/engine-edit/engine-edit.component';
 
 const routes: Routes = [
 
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {
     path: 'home', component: HomeComponent
+  },
+  {
+    path: 'parts',
+    children: [
+      {path: '', component: PartsComponent},
+      {
+        path: 'engines',
+        children: [
+          {path: '', component: EnginesComponent},
+          {path: 'add', component: EngineAddComponent},
+          {path: 'detail/:id', component: EngineEditComponent}
+        ]
+      }
+    ]
+  },
+  {
+    path: 'cars', component: CarsComponent
+  },
+  {
+    path: 'orders', component: OrdersComponent
   }
 
 ];
--- carly-web/src/app/carly-shared/carly-shared.module.ts
+++ carly-web/src/app/carly-shared/carly-shared.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { PartsComponent } from './components/parts/parts.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { EnginesComponentComponent } from './components/engines-component/engines-component.component';
+import { EnginesComponent } from './components/engines/engines.component';
+import { EnginesAddComponent } from './components/engines/engines-add/engines-add.component';
+import { EngineAddComponent } from './components/engines/engine-add/engine-add.component';
+import { EngineEditComponent } from './components/engines/engine-edit/engine-edit.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PartsComponent, CarsComponent, OrdersComponent, EnginesComponentComponent, EnginesComponent, EnginesAddComponent, EngineAddComponent, EngineEditComponent],
   imports: [
     CommonModule
   ]
