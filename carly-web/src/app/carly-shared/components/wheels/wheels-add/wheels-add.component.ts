a5a75dc9d4125b0ea7e6f609d87dcac00202d60b
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-21 20:51:52 +0200

    Init wheels component.

---
+++ carly-web/src/app/carly-shared/components/wheels/wheels-add/wheels-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wheels-add',
+  templateUrl: './wheels-add.component.html',
+  styleUrls: ['./wheels-add.component.scss']
+})
+export class WheelsAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ carly-web/src/app/carly-shared/components/wheels/wheels-edit/wheels-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wheels-edit',
+  templateUrl: './wheels-edit.component.html',
+  styleUrls: ['./wheels-edit.component.scss']
+})
+export class WheelsEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
