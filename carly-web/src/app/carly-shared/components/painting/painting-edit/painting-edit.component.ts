583a3272c87dfcb268b68de49425ba13c340a1fb
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-21 20:50:43 +0200

    Implement painting component.

---
+++ carly-web/src/app/carly-shared/components/painting/painting-edit/painting-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-painting-edit',
+  templateUrl: './painting-edit.component.html',
+  styleUrls: ['./painting-edit.component.scss']
+})
+export class PaintingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
