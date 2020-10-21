05fa5c7d90368a9b63731fb9a8d31cb2de316dfe
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-21 20:52:19 +0200

    Init windows component.

---
+++ carly-web/src/app/carly-shared/components/windows/windows-add/windows-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-windows-add',
+  templateUrl: './windows-add.component.html',
+  styleUrls: ['./windows-add.component.scss']
+})
+export class WindowsAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ carly-web/src/app/carly-shared/components/windows/windows-edit/windows-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-windows-edit',
+  templateUrl: './windows-edit.component.html',
+  styleUrls: ['./windows-edit.component.scss']
+})
+export class WindowsEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
