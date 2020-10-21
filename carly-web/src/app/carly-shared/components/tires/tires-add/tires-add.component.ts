f0a6b5fbd8292879277fcdf3e59530d2e11182a3
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-21 20:51:30 +0200

    Implement tires component.

---
+++ carly-web/src/app/carly-shared/components/tires/tires-add/tires-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tires-add',
+  templateUrl: './tires-add.component.html',
+  styleUrls: ['./tires-add.component.scss']
+})
+export class TiresAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ carly-web/src/app/carly-shared/components/tires/tires-edit/tires-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tires-edit',
+  templateUrl: './tires-edit.component.html',
+  styleUrls: ['./tires-edit.component.scss']
+})
+export class TiresEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
