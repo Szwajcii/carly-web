05959aa8620582d6e0f3fcff2d4e7bf079f51a5c
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-22 20:35:54 +0200

    Init CarModel model.

---
+++ carly-web/src/app/carly-shared/model/car-model.model.ts
@@ -0,0 +1,30 @@
+import {Page} from './paginated.model';
+import {Brand} from './brand.model';
+
+export namespace CarModel {
+
+  export interface Model {
+    id: string;
+    brand: Brand;
+    name: string;
+    variants: Array<CarVariant>;
+  }
+
+  export type PaginatedModel = Page<Model>;
+
+  export type POST = Model;
+
+  export type PUT = Model;
+
+  export enum CarVariant {
+    Kombi = 'Kombi',
+    Sedan = 'Sedan',
+    Hatchback = 'Hatchback',
+    Compact = 'Compact',
+    SUV = 'SUV',
+    Minivan = 'Minivan',
+    Cabriolet = 'Cabriolet',
+    Coupe = 'Coupe'
+  }
+
+}
