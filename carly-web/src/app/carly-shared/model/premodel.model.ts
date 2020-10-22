92332b58629438ebf6beb1176af123eb0b007522
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-22 20:39:05 +0200

    init PreModel with common fields.

---
+++ carly-web/src/app/carly-shared/model/premodel.model.ts
@@ -0,0 +1,10 @@
+import {Brand} from './brand.model';
+
+export class PreModel {
+  id: string;
+  name: string;
+  brand: Brand;
+  preview?: string;
+  price: number;
+  createdDate: Date | string;
+}
