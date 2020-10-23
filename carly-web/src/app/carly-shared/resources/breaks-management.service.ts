cb260de22e7134e1a243e38884d24fda7762c857
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-23 08:29:50 +0200

    Init breaks management service.

---
+++ carly-web/src/app/carly-shared/resources/breaks-management.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Breaks} from '../model/breaks.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BreaksManagementService {
+
+  constructor(protected http: HttpClient) {
+  }
+
+  // breaksManagementApi: string = `${BASE_API_URL}`;
+  breaksManagementApi = 'api/breaks';
+
+  // BREAKS
+  findAll(params: HttpParams): Observable<Breaks.PaginatedModel> {
+    return this.http.get<Breaks.PaginatedModel>(`${this.breaksManagementApi}`, {params});
+  }
+
+}
