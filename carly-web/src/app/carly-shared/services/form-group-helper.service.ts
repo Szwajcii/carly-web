d757bda53410015f5bffbdf4a288c872f641335e
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-24 18:46:11 +0200

    Add method to get controls in FormGroupHelper service.

--- carly-web/src/app/carly-shared/services/form-group-helper.service.ts
+++ carly-web/src/app/carly-shared/services/form-group-helper.service.ts
@@ -1,9 +1,30 @@
 import {Injectable} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {FormGroupHelper} from '../model/form-group-helper.model';
 
 @Injectable()
 export class FormGroupHelperService {
 
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  addControlToModel(model: FormGroupHelper.Model[]): FormGroupHelper.ModelControl[] {
+    return model.map(input => {
+      if (input.group) {
+        const controls = input.inputs
+          .map(nestedInputs => ({...nestedInputs, control: new FormControl('', nestedInputs.validators)}));
+        return {...input, control: this.formBuilder.group(this.getControlsFromModel(controls))};
+      } else {
+        return {...input, control: new FormControl('', input.validators)};
+      }
+    });
+  }
+
+  getControlsFromModel(modelControl: FormGroupHelper.ModelControl[]): FormGroupHelper.Controls {
+    return modelControl
+      .reduce((obj, item) => ({...obj, [item.inputName]: item.control}), {});
+  }
 
   // Helper method to debug form controls
   public findInvalidControls(formGroup: FormGroup) {
