92ac1bb1a27cf3e0aed6eb3d58bdd602a53dfe28
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-24 21:50:44 +0200

    Init WheelsManagementService.

---
+++ carly-web/src/app/carly-shared/resources/wheels-management.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Wheels} from '../model/wheels.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WheelsManagementService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  wheelsManagementApi = 'api/wheels';
+
+  findAll(params: HttpParams): Observable<Wheels.PaginatedModel> {
+    return this.http.get<Wheels.PaginatedModel>(`${this.wheelsManagementApi}`, {params});
+  }
+
+  findById(id: string): Observable<Wheels.Model> {
+    return this.http.get<Wheels.Model>(`${this.wheelsManagementApi}/${id}`);
+  }
+
+  create(model: Wheels.POST): Observable<Wheels.Model> {
+    return this.http.post<Wheels.Model>(`${this.wheelsManagementApi}`, model);
+  }
+
+  update(model: Wheels.PUT): Observable<Wheels.Model> {
+    return this.http.put<Wheels.Model>(`${this.wheelsManagementApi}`, model);
+  }
+
+}
