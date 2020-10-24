4e01f3e47e3aa5b0e787aab77c9f3c4708c62b57
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-24 17:25:25 +0200

    Add method in breaks management service.

--- carly-web/src/app/carly-shared/resources/breaks-management.service.ts
+++ carly-web/src/app/carly-shared/resources/breaks-management.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Breaks} from '../model/breaks.model';
@@ -17,6 +17,10 @@ export class BreaksManagementService {
   // BREAKS
   findAll(params: HttpParams): Observable<Breaks.PaginatedModel> {
     return this.http.get<Breaks.PaginatedModel>(`${this.breaksManagementApi}`, {params});
+  }
+
+  findById(id: string): Observable<Breaks.Model> {
+    return this.http.get<Breaks.Model>(`${this.breaksManagementApi}/${id}`);
   }
 
 }
