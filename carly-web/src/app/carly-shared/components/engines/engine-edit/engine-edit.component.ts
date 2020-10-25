0416e74edf4f4472c1738c311ad3d715b66bf084
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-25 12:08:34 +0100

    Implement methods in engine edit component.

--- carly-web/src/app/carly-shared/components/engines/engine-edit/engine-edit.component.ts
+++ carly-web/src/app/carly-shared/components/engines/engine-edit/engine-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {map, mergeMap} from 'rxjs/operators';
+
+import {Engine} from '../../../model/engine.model';
+import {PartFormAction} from '../../../model/part-form-action.model';
+import {EngineManagementService} from '../../../resources/engine-management.service';
 
 @Component({
   selector: 'app-engine-edit',
@@ -7,9 +14,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EngineEditComponent implements OnInit {
 
-  constructor() { }
+  engineModel: Engine.Model;
+  formAction = PartFormAction.EDIT;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private engineManagementService: EngineManagementService
+  ) {
+  }
 
   ngOnInit(): void {
+
+    this.findEngineById()
+      .pipe(
+        mergeMap(id => this.engineManagementService.findById(id)),
+        map(model => {
+          console.log(200, model);
+          return model;
+        })
+      ).subscribe(model => {
+      this.engineModel = model;
+    });
+
+  }
+
+  private findEngineById(): Observable<string> {
+    return this.activatedRoute.params.pipe(map(params => params.id));
   }
 
 }
