babd6d4a5b74a5c504350fcc1ef68c6698a060de
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-25 15:23:07 +0100

    Implement TiresManagementService.

---
+++ carly-web/src/app/carly-shared/resources/tires-management.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Tires} from '../model/tires.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TiresManagementService {
+
+  tiresManagementApi: 'api/tires';
+
+  constructor(private http: HttpClient) {
+  }
+
+  findAll(params: HttpParams): Observable<Tires.PaginatedModel> {
+    return this.http.get<Tires.PaginatedModel>(`${this.tiresManagementApi}`, {params});
+  }
+
+  findById(id: string): Observable<Tires.Model> {
+    return this.http.get<Tires.Model>(`${this.tiresManagementApi}/${id}`);
+  }
+
+  create(model: Tires.POST): Observable<Tires.Model> {
+    return this.http.post<Tires.Model>(`${this.tiresManagementApi}`, model);
+  }
+
+  update(model: Tires.PUT): Observable<Tires.Model> {
+    return this.http.put<Tires.Model>(`${this.tiresManagementApi}`, model);
+  }
+
+}
