eb1b348653fbaf00a06815e35c5761cc30d18af7
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-25 11:28:28 +0100

    Add methods to WheelsForm. Use PartForm in wheels form component.

--- carly-web/src/app/carly-shared/components/wheels/wheels-form/wheels-form.component.ts
+++ carly-web/src/app/carly-shared/components/wheels/wheels-form/wheels-form.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+
+import {Wheels} from '../../../model/wheels.model';
+import {ValueLabel} from '../../../model/value-label';
+import {PartFormAction} from '../../../model/part-form-action.model';
+import {MessageService} from '../../../services/message.service';
+import {FormGroupHelperService} from '../../../services/form-group-helper.service';
+import {WheelsManagementService} from '../../../resources/wheels-management.service';
+import {wheelsDetailsFormFields, wheelsPreviews} from '../wheels-form-fields';
 
 @Component({
   selector: 'app-wheels-form',
@@ -7,9 +17,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WheelsFormComponent implements OnInit {
 
-  constructor() { }
+  private static PREVIEW = 'preview';
+
+  @Input() isDisabled = false;
+  @Input() formAction: PartFormAction;
+  @Input() wheelsModel: Wheels.Model;
+  @Input() submitEvent: EventEmitter<Wheels.Model> = new EventEmitter<Wheels.Model>();
+  @Input() details = false;
+
+  wheelsPreview: Array<ValueLabel>;
+  wheelsDetailsForm: FormGroup;
+  wheelsDetailsFormControls = this.formGroupService.addControlToModel(wheelsDetailsFormFields)
+    .map(controlModel => {
+      if (controlModel.inputName === WheelsFormComponent.PREVIEW) {
+        controlModel.selectOptions = wheelsPreviews;
+      }
+      return controlModel;
+    });
+
+  constructor(
+    private messageService: MessageService,
+    private formGroupService: FormGroupHelperService,
+    private router: Router,
+    private wheelsManagementService: WheelsManagementService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.wheelsPreview = wheelsPreviews;
+  }
+
+  onSubmit($event) {
+    this.wheelsDetailsForm = $event;
+
+    const wheels: Wheels.Model = {
+      ...this.wheelsDetailsForm.value
+    };
+
+    this.createOrUpdate(wheels);
+  }
+
+  createOrUpdate(wheels: Wheels.Model) {
+    let partAction;
+
+    if (this.formAction === PartFormAction.CREATE) {
+      partAction = this.wheelsManagementService.create(wheels);
+    } else {
+      partAction = this.wheelsManagementService.update(wheels);
+    }
+
+    partAction.subscribe(data => {
+      this.messageService.showMessage('Wheels created!');
+      this.submitEvent.emit(wheels);
+      this.router.navigate(['/parts/wheels', 'details', data.id, 'edit']);
+    }, error => {
+      this.messageService.showMessage('Create wheels failed!');
+      console.log(error);
+    });
   }
 
 }
