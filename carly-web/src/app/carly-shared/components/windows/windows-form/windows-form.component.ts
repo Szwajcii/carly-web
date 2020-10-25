346902e78b709602d74f9226f8d4cd5d5ca9d194
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-25 13:13:00 +0100

    Add methods to WindowsFormComponent.

--- carly-web/src/app/carly-shared/components/windows/windows-form/windows-form.component.ts
+++ carly-web/src/app/carly-shared/components/windows/windows-form/windows-form.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormGroup} from '@angular/forms';
+
+import {Windows} from '../../../model/windows.model';
+import {ValueLabel} from '../../../model/value-label';
+import {PartFormAction} from '../../../model/part-form-action.model';
+import {MessageService} from '../../../services/message.service';
+import {FormGroupHelperService} from '../../../services/form-group-helper.service';
+import {WindowsManagementService} from '../../../resources/windows-management.service';
+import {windowsDetailsFormFields, windowsPreviews} from '../windows-form-fields';
 
 @Component({
   selector: 'app-windows-form',
@@ -7,9 +17,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WindowsFormComponent implements OnInit {
 
-  constructor() { }
+  private static PREVIEW = 'preview';
+
+  @Input() isDisabled = false;
+  @Input() formAction: PartFormAction;
+  @Input() windowsModel: Windows.Model;
+  @Input() submitEvent: EventEmitter<Windows.Model> = new EventEmitter<Windows.Model>();
+  @Input() details = false;
+
+  windowsPreviews: Array<ValueLabel>;
+  windowsDetailsForm: FormGroup;
+  windowsDetailsFormControls = this.formGroupService.addControlToModel(windowsDetailsFormFields)
+    .map(controlModel => {
+      if (controlModel.inputName === WindowsFormComponent.PREVIEW) {
+        controlModel.selectOptions = windowsPreviews;
+      }
+      return controlModel;
+    });
+
+  constructor(
+    private messageService: MessageService,
+    private formGroupService: FormGroupHelperService,
+    private router: Router,
+    private windowsManagementService: WindowsManagementService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.windowsPreviews = windowsPreviews;
+  }
+
+  onSubmit($event) {
+    this.windowsDetailsForm = $event;
+
+    const windows: Windows.Model = {
+      ...this.windowsDetailsForm.value
+    };
+
+    this.createOrUpdate(windows);
+
+  }
+
+  createOrUpdate(windows: Windows.Model) {
+    let partAction;
+
+    if (this.formAction === PartFormAction.CREATE) {
+      partAction = this.windowsManagementService.create(windows);
+    } else {
+      partAction = this.windowsManagementService.update(windows);
+    }
+
+    partAction.subscribe(data => {
+      this.messageService.showMessage('Windows created!');
+      this.submitEvent.emit(windows);
+      this.router.navigate(['/parts/windows', 'details', data.id, 'edit']);
+    }, error => {
+      this.messageService.showMessage('Create window failed!');
+      console.log(error);
+    });
   }
 
 }
