54f66f6db1e500504c1632c7a13f674aef898a35
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-25 13:12:25 +0100

    Implement WindowsManagementService.

---
+++ carly-web/src/app/carly-shared/resources/windows-management.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Windows} from '../model/windows.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WindowsManagementService {
+
+  windowsManagementApi = 'api/windows';
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  findAll(params: HttpParams): Observable<Windows.PaginatedModel> {
+    return this.http.get<Windows.PaginatedModel>(`${this.windowsManagementApi}`, {params});
+  }
+
+  findById(id: string): Observable<Windows.Model> {
+    return this.http.get<Windows.Model>(`${this.windowsManagementApi}/${id}`);
+  }
+
+  create(model: Windows.POST): Observable<Windows.Model> {
+    return this.http.post<Windows.Model>(`${this.windowsManagementApi}`, model);
+  }
+
+  update(model: Windows.PUT): Observable<Windows.Model> {
+    return this.http.put<Windows.Model>(`${this.windowsManagementApi}`, model);
+  }
+
+}
