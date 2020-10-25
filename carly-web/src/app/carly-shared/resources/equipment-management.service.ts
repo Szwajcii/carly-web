15aeebabdc8c2e9f79a58e2b5e9ba2a89e7d9626
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-25 19:37:10 +0100

    Implement EquipmentManagementService.

---
+++ carly-web/src/app/carly-shared/resources/equipment-management.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import {Equipment} from '../model/equipment.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EquipmentManagementService {
+
+  equipmentManagementApi = 'api/equipment';
+
+  constructor(private http: HttpClient) {
+  }
+
+  findAll(params: HttpParams): Observable<Equipment.PaginatedModel> {
+    return this.http.get<Equipment.PaginatedModel>(`${this.equipmentManagementApi}`, {params});
+  }
+
+  findById(id: string): Observable<Equipment.Model> {
+    return this.http.get<Equipment.Model>(`${this.equipmentManagementApi}/${id}`);
+  }
+
+  create(model: Equipment.POST): Observable<Equipment.Model> {
+    return this.http.post<Equipment.Model>(`${this.equipmentManagementApi}`, model);
+  }
+
+  update(model: Equipment.PUT): Observable<Equipment.Model> {
+    return this.http.put<Equipment.Model>(`${this.equipmentManagementApi}`, model);
+  }
+
+}
