d2516609207e9033a172c7580511fa95d05cea4a
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-26 18:58:02 +0100

    Add equipment form fields and equipment previews.

---
+++ carly-web/src/app/carly-shared/components/equipment/equipment-form-fields.ts
@@ -0,0 +1,57 @@
+import {Validators} from '@angular/forms';
+import {ValueLabel} from '../../model/value-label';
+import {FormGroupHelper} from '../../model/form-group-helper.model';
+import {Equipment} from '../../model/equipment.model';
+
+export const equipmentDetailsFormFields: FormGroupHelper.Model[] = [
+  {
+    inputName: 'partName',
+    label: 'Name',
+    validators: [Validators.required],
+    type: 'text',
+    cols: 4,
+    rows: 1
+  },
+  {
+    inputName: 'partBrand',
+    label: 'Brand',
+    validators: [Validators.required],
+    type: 'select',
+    cols: 4,
+    rows: 1,
+    selectOptions: []
+  },
+  {
+    inputName: 'type',
+    label: 'Type',
+    validators: [Validators.required],
+    type: 'select',
+    cols: 4,
+    rows: 1,
+    selectOptions: [...Object.values(Equipment.EquipmentType).map(value => ({label: value, value}))]
+  },
+  {
+    inputName: 'partPrice',
+    label: 'Price',
+    validators: [Validators.required],
+    type: 'number',
+    cols: 4,
+    rows: 1
+  },
+  {
+    inputName: 'preview',
+    label: 'Preview',
+    validators: [Validators.required],
+    type: 'select',
+    cols: 4,
+    rows: 1,
+    selectOptions: []
+  }
+];
+
+export const equipmentPreviews: Array<ValueLabel> = [
+  {
+    value: 'equipment.png',
+    label: 'Preview 1'
+  }
+];
