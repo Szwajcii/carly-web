ed44637f19d638569733149d2978885a144efaf4
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-28 21:54:19 +0100

    Implement header component with navigation bar.

--- carly-web/src/app/app.module.ts
+++ carly-web/src/app/app.module.ts
@@ -7,11 +7,13 @@ import {CarlySharedModule} from './carly-shared/carly-shared.module';
 import { AuthComponent } from './auth/auth.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ carly-web/src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  isAuthenticated = false;
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
+
+  ngOnDestroy(): void {
+  }
+
+}
