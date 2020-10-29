fe71c28ee88c2aa047bab4c05f50675bde0e2358
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-10-29 08:53:32 +0100

    Implement AuthInterceptor. Add it to providers.

--- carly-web/src/app/app.module.ts
+++ carly-web/src/app/app.module.ts
@@ -6,8 +6,9 @@ import {AppComponent} from './app.component';
 import {CarlySharedModule} from './carly-shared/carly-shared.module';
 import { AuthComponent } from './auth/auth.component';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import {AuthInterceptorService} from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,11 @@ import { HeaderComponent } from './header/header.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
