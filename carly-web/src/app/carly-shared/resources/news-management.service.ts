dcb8c9e462f746201ee6ad25bf7066ae28da3852
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-02 17:27:25 +0100

    Add methods in News management service.

--- carly-web/src/app/carly-shared/resources/news-management.service.ts
+++ carly-web/src/app/carly-shared/resources/news-management.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {News} from '../model/news.model';
 
@@ -15,6 +15,26 @@ export class NewsManagementService {
 
   findLatestNews(): Observable<News.Model> {
     return this.http.get<News.Model>(`${this.newsManagementApi}/latest`);
+  }
+
+  findAll(params: HttpParams): Observable<News.PaginatedModel> {
+    return this.http.get<News.PaginatedModel>(`${this.newsManagementApi}`, {params});
+  }
+
+  findById(id: string): Observable<News.Model> {
+    return this.http.get<News.Model>(`${this.newsManagementApi}/${id}`);
+  }
+
+  create(model: News.POST): Observable<News.Model> {
+    return this.http.post<News.Model>(`${this.newsManagementApi}`, model);
+  }
+
+  update(model: News.PUT): Observable<News.Model> {
+    return this.http.put<News.Model>(`${this.newsManagementApi}`, model);
+  }
+
+  delete(id: string) {
+    this.http.delete(`${this.newsManagementApi}/delete/${id}`);
   }
 
 }
