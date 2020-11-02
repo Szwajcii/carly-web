1b1f6564f8f19f1e2e7c4e01860fe54231f3ee24
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-02 20:30:20 +0100

    Add methods in UserManagementService.

--- carly-web/src/app/carly-shared/resources/user-management.service.ts
+++ carly-web/src/app/carly-shared/resources/user-management.service.ts
@@ -1,5 +1,12 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+import {AuthService} from '../../auth/auth.service';
+import {Roles} from '../model/roles.model';
+import {UserContext} from '../model/user-context.model';
+import {User} from '../model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +15,22 @@ export class UserManagementService {
 
   userManagementApi = 'api/users';
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {
   }
 
+  findAll(params: HttpParams): Observable<User> {
+    return null;
+  }
+
+  // FIXME: After connection with BE replace UserContext with User model!!!
+  getUserContext(): Observable<UserContext> {
+    return this.authService.userContext;
+  }
+
+  isUserHasRole(role: Roles): Observable<boolean> {
+    return this.getUserContext().pipe(map(user => user.role === role));
+  }
 }
