03cb839cf11f194d8926ccba62e5dff499919123
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-02 17:29:49 +0100

    Implement Company management and User management services.

---
+++ carly-web/src/app/carly-shared/resources/company-management.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Company} from '../model/company.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyManagementService {
+
+  companyManagementApi = 'api/company';
+
+  constructor(private http: HttpClient) {
+  }
+
+  findAll(params: HttpParams): Observable<Company.PaginatedModel> {
+    return this.http.get<Company.PaginatedModel>(`${this.companyManagementApi}`, {params});
+  }
+
+  findById(id: string): Observable<Company.Model> {
+    return this.http.get<Company.Model>(`${this.companyManagementApi}/${id}`);
+  }
+
+  create(model: Company.POST): Observable<Company.Model> {
+    return this.http.post<Company.Model>(`${this.companyManagementApi}`, model);
+  }
+
+  update(model: Company.PUT): Observable<Company.Model> {
+    return this.http.put<Company.Model>(`${this.companyManagementApi}`, model);
+  }
+
+  delete(id: string) {
+    this.http.delete(`${this.companyManagementApi}/delete/${id}`);
+  }
+
+}
