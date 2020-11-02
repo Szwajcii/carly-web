1e56db3966bb51943665f6e5cae23d6bb2b6b4a9
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-02 17:36:17 +0100

    Implement lazy loading in app routing module.

--- carly-web/src/app/app-routing.module.ts
+++ carly-web/src/app/app-routing.module.ts
@@ -1,56 +1,36 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './carly-shared/components/home/home.component';
-import {PartsComponent} from './carly-shared/components/parts/parts.component';
-import {CarsComponent} from './carly-shared/components/cars/cars.component';
-import {OrdersComponent} from './carly-shared/components/orders/orders.component';
-import {EnginesComponent} from './carly-shared/components/engines/engines.component';
-import {EngineAddComponent} from './carly-shared/components/engines/engine-add/engine-add.component';
-import {EngineEditComponent} from './carly-shared/components/engines/engine-edit/engine-edit.component';
-import {BreaksComponent} from './carly-shared/components/breaks/breaks.component';
-import {BreaksEditComponent} from './carly-shared/components/breaks/breaks-edit/breaks-edit.component';
-import {BreaksAddComponent} from './carly-shared/components/breaks/breaks-add/breaks-add.component';
 import {AuthComponent} from './auth/auth.component';
 import {AuthGuard} from './auth/auth.guard';
 import {CarlyRoleGuard} from './carly-shared/services/guards/carly-role-guard';
 import {Roles} from './carly-shared/model/roles.model';
+import {CarlyMatcher} from './carly-shared/services/matcher';
 
 const routes: Routes = [
-
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'parts',
-    component: PartsComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'breaks',
-    component: BreaksComponent,
+    path: '',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     children: [
-      {path: 'add', component: BreaksAddComponent},
-      {path: 'details/:id', component: BreaksEditComponent}
+      {
+        canActivate: [CarlyRoleGuard],
+        data: {roles: [Roles.CARLY_ADMINISTRATOR]},
+        matcher: CarlyMatcher.carlyAdministratorMatcher,
+        loadChildren: () => import('./carly-administrator/carly-administrator.module').then(module => module.CarlyAdministratorModule)
+      },
+      {
+        canActivate: [CarlyRoleGuard],
+        data: {roles: [Roles.CARLY_COMPANY]},
+        matcher: CarlyMatcher.carlyCompanyMatcher,
+        loadChildren: () => import('./carly-company/carly-company.module').then(module => module.CarlyCompanyModule)
+      },
+      {
+        canActivate: [CarlyRoleGuard],
+        data: {roles: [Roles.CARLY_CUSTOMER]},
+        matcher: CarlyMatcher.carlyCustomerMatcher,
+        loadChildren: () => import('./carly-customer/carly-customer.module').then(module => module.CarlyCustomerModule)
+      }
     ]
-  },
-  {
-    path: 'engines',
-    children: [
-      {path: '', component: EnginesComponent},
-      {path: 'add', component: EngineAddComponent},
-      {path: 'detail/:id', component: EngineEditComponent}
-    ]
-  },
-  {
-    path: 'cars', component: CarsComponent
-  },
-  {
-    path: 'orders',
-    component: OrdersComponent,
-    canActivate: [CarlyRoleGuard],
-    data: {roles: [Roles.CARLY_CUSTOMER]}
   },
   {
     path: 'auth', component: AuthComponent
