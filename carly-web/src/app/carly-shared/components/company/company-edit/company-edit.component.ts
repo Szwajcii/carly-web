cd100e6b696b2df532cfde930fe887e1a758462a
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-03 08:42:35 +0100

    Start implementing methods in company edit.

--- carly-web/src/app/carly-shared/components/company/company-edit/company-edit.component.ts
+++ carly-web/src/app/carly-shared/components/company/company-edit/company-edit.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {map, mergeMap} from 'rxjs/operators';
+import {Company} from '../../../model/company.model';
+import {CompanyManagementService} from '../../../resources/company-management.service';
+import {UserManagementService} from '../../../resources/user-management.service';
+import {Roles} from '../../../model/roles.model';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-company-edit',
@@ -7,9 +15,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyEditComponent implements OnInit {
 
-  constructor() { }
+  isDisabled = true;
+  company: Company.Model;
 
-  ngOnInit(): void {
+  constructor(
+    private companyManagementService: CompanyManagementService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private userManagementService: UserManagementService
+  ) {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  findCompanyId() {
+    // todo: Try getting id of company from context first
+
+    return this.userManagementService.isUserHasRole(Roles.CARLY_COMPANY)
+      .pipe(
+        mergeMap(hasRole => {
+          if (hasRole) {
+            // todo: Check if company should subscribe!!! Maybe getUserContext should not return Observable?
+            return of(this.userManagementService.getUserContext())
+              .pipe(map(company => company.subscribe(data => data.id)));
+          }
+          return this.activatedRoute.parent.params.pipe(map(params => params.id));
+        })
+      );
+  }
 }
