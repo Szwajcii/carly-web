c613b26ded5d0d3e13790a97ed961c57f55cf0f3
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-05 18:10:48 +0100

    Import components and refactor AppModule.

--- carly-web/src/app/app.module.ts
+++ carly-web/src/app/app.module.ts
@@ -1,35 +1,39 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CarlySharedModule} from './carly-shared/carly-shared.module';
-import { AuthComponent } from './auth/auth.component';
-import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
+import {HeaderComponent} from './header/header.component';
+import {AuthInterceptorService} from './carly-shared/components/auth/auth-interceptor.service';
+import {CarlyMatModule} from './carly-shared/modules/carly-mat.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthComponent,
-    HeaderComponent
-  ],
+    declarations: [
+        AppComponent,
+        HeaderComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarlySharedModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    CarlySharedModule,
+    BrowserAnimationsModule
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
-  ],
-  bootstrap: [AppComponent]
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptorService,
+            multi: true
+        }
+    ],
+    exports: [
+        CarlyMatModule,
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
