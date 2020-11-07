b7f71cfbf6b2b9d5dab6a4e2b0a8c310f97fa97f
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-07 15:54:47 +0100

    Add fields in registration model.

--- carly-web/src/app/carly-shared/model/registration.model.ts
+++ carly-web/src/app/carly-shared/model/registration.model.ts
@@ -1,30 +1,26 @@
 import {Address} from './address.model';
+import {Gender} from './gender.enum';
 
 export namespace Registration {
 
   export interface Model extends CustomerModel, CompanyModel {
     email: string;
     password: string;
-    repeatPassword: string;
+    rePassword: string;
+    gender: Gender;
+    phone: string;
   }
 
   export interface CustomerModel {
     firstName: string;
     lastName: string;
-    gender: Gender;
   }
 
   export interface CompanyModel {
     companyName: string;
-    phoneNumber: string;
     address?: Address;
   }
 
   export type POST = Model;
-
-  export enum Gender {
-    MALE = 'MALE',
-    FEMALE = 'FEMALE'
-  }
 
 }
