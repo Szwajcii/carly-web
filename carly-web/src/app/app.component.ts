f33078ffaf23b1f6792ea42e7de01736a6b3350e
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-08 12:42:06 +0100

    Reimport AuthService.

--- carly-web/src/app/app.component.ts
+++ carly-web/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from './auth/auth.service';
+import {AuthService} from './carly-shared/components/auth/auth.service';
 
 @Component({
   selector: 'app-root',
