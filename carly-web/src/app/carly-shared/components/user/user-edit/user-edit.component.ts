be8c5429381998c09ae52a33adeaf55e75da1a8a
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-10 19:37:22 +0100

    Implement UserFormComponent. Changes in UserEditComponent.

--- carly-web/src/app/carly-shared/components/user/user-edit/user-edit.component.ts
+++ carly-web/src/app/carly-shared/components/user/user-edit/user-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {map, mergeMap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+import {UserManagementService} from '../../../resources/user-management.service';
+import {User} from '../../../model/user.model';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  isDisabled = true;
+  user: User;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userManagementService: UserManagementService
+  ) {
+  }
 
   ngOnInit(): void {
+
+    this.findUserById()
+      .pipe(
+        mergeMap(id => this.userManagementService.findById(id)),
+        map(model => {
+          console.log(200, model);
+          return model;
+        })
+      ).subscribe(userModel => {
+      this.user = userModel;
+    });
+
   }
+
+  findUserById(): Observable<string> {
+    if (this.userManagementService.getUserContext() != null) {
+      return this.userManagementService.getUserContext().pipe(map(r => {
+        return r.id;
+      }));
+    }
+    return this.activatedRoute.params.pipe(map(params => params.id));
+  }
+
 
 }
