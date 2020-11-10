7bd097df5ed0abf5f42e73bef6590ecd3c2aecfc
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-10 19:34:04 +0100

    Change directory and add cols and rows in UserFormFields.

--- carly-web/src/app/carly-shared/components/user/user-form/user-form-fields.ts
+++ carly-web/src/app/carly-shared/components/user/user-form/user-form-fields.ts
@@ -1,30 +1,38 @@
 import {Validators} from '@angular/forms';
-import {FormGroupHelper} from '../../model/form-group-helper.model';
+import {FormGroupHelper} from '../../../model/form-group-helper.model';
 
 export const userFormFields: FormGroupHelper.Model[] = [
   {
     inputName: 'firstName',
     label: 'First name',
     validators: [Validators.required],
-    type: 'text'
+    type: 'text',
+    cols: 4,
+    rows: 1
   },
   {
     inputName: 'lastName',
     label: 'Last name',
     validators: [Validators.required],
-    type: 'text'
+    type: 'text',
+    cols: 4,
+    rows: 1
   },
   {
     inputName: 'gender',
     label: 'Gender',
     validators: [Validators.required],
     type: 'select',
-    selectOptions: []
+    selectOptions: [],
+    cols: 4,
+    rows: 1
   },
   {
     inputName: 'phoneNumber',
     label: 'Phone number',
     validators: [Validators.required],
-    type: 'text'
+    type: 'text',
+    cols: 4,
+    rows: 1
   }
 ];
