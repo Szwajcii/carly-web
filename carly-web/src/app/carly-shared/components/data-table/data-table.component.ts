ad5b4ee8febcd34daedaf7df869dbf8c47452417
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-15 13:29:24 +0100

    Implement DataTableComponent.

---
+++ carly-web/src/app/carly-shared/components/data-table/data-table.component.ts
@@ -0,0 +1,45 @@
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements OnInit, AfterViewInit {
+
+  public datasource = new MatTableDataSource([]);
+  @ViewChild('paginator', {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  @Input() set data(data: any[]) {
+    this.setDatasource(data);
+  }
+
+  @Input() displayedColumns: Array<string>;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
+  }
+
+  setDatasource(data: any[]) {
+    this.datasource = new MatTableDataSource<any>(data);
+    this.datasource.data = data;
+    this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.datasource.filter = filterValue.trim().toLowerCase();
+  }
+
+}
