a08445be00cb98f1b2ca862e49b9ebce8e71f8b5
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-15 13:31:16 +0100

    Add data table in WindowsComponent.

--- carly-web/src/app/carly-shared/components/windows/windows.component.ts
+++ carly-web/src/app/carly-shared/components/windows/windows.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Roles} from '../../model/roles.model';
+import {Windows} from '../../model/windows.model';
+import {WindowsManagementService} from '../../resources/windows-management.service';
 
 @Component({
   selector: 'app-windows',
@@ -7,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WindowsComponent implements OnInit {
 
-  constructor() { }
+  CarlyCompany = Roles.CARLY_COMPANY;
+  CarlyAdministrator = Roles.CARLY_ADMINISTRATOR;
+
+  windows: Windows.Model[];
+  loading = true;
+
+  public displayedColumns: Array<string> = [
+    'name',
+    'price'
+  ];
+
+  constructor(
+    private windowsManagementService: WindowsManagementService
+  ) {
+  }
 
   ngOnInit(): void {
   }
