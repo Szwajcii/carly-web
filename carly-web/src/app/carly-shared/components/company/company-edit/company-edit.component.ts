4a4c20a2146fe795ce9bf40d5e74194ce3d93ac6
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-18 08:24:44 +0100

    Load company data from context.

--- carly-web/src/app/carly-shared/components/company/company-edit/company-edit.component.ts
+++ carly-web/src/app/carly-shared/components/company/company-edit/company-edit.component.ts
@@ -6,8 +6,9 @@ import {Company} from '../../../model/company.model';
 import {CompanyManagementService} from '../../../resources/company-management.service';
 import {UserManagementService} from '../../../resources/user-management.service';
 import {Roles} from '../../../model/roles.model';
-import {of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {FormAction} from '../../../model/form-action.model';
+import {MessageService} from '../../../services/message.service';
 
 @Component({
   selector: 'app-company-edit',
@@ -24,23 +25,33 @@ export class CompanyEditComponent implements OnInit {
     private companyManagementService: CompanyManagementService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private userManagementService: UserManagementService
+    private userManagementService: UserManagementService,
+    private messageService: MessageService
   ) {
   }
 
   ngOnInit(): void {
+    this.findCompanyId()
+      .pipe(
+        mergeMap(id => this.companyManagementService.findById(id)),
+        map(model => {
+          return model;
+        })
+      ).subscribe(model => {
+        this.company = model;
+      }, error => {
+        this.messageService.showMessage('Unexpected error has occurred!');
+        console.log(error);
+    });
   }
 
-  findCompanyId() {
-    // todo: Try getting id of company from context first
-
+  findCompanyId(): Observable<string> {
     return this.userManagementService.isUserHasRole(Roles.CARLY_COMPANY)
       .pipe(
         mergeMap(hasRole => {
           if (hasRole) {
-            // todo: Check if company should subscribe!!! Maybe getUserContext should not return Observable?
-            return of(this.userManagementService.getUserContext())
-              .pipe(map(company => company.subscribe(data => data.id)));
+            return this.userManagementService.getUserContext()
+              .pipe(map(data => data.id));
           }
           return this.activatedRoute.parent.params.pipe(map(params => params.id));
         })
