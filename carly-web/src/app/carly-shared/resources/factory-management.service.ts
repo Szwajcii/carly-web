8fbdebdd7ee6bb2b2244b70c848f9fcb184857b7
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-27 08:27:36 +0100

    Implement service for factory matching.

---
+++ carly-web/src/app/carly-shared/resources/factory-management.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FactoryMatch} from '../model/factory-match.model';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FactoryManagementService {
+
+  factoryManagementApi = 'api/factory';
+
+  constructor(private http: HttpClient) {
+  }
+
+  // todo: Pass company id to BE
+  findFactoryMatching(): Observable<FactoryMatch[]> {
+    return this.http.get<FactoryMatch[]>(`${this.factoryManagementApi}`);
+  }
+
+  requestMatching(factoryId: string) {
+    return null;
+  }
+}
