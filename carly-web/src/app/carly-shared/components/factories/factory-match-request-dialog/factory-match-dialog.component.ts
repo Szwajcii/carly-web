167d82337bbce1c100b47d135a6b4bdf6d121769
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-29 11:26:08 +0100

    Rename factory match dialog.

--- carly-web/src/app/carly-shared/components/factories/factory-match-request-dialog/factory-match-dialog.component.ts
+++ carly-web/src/app/carly-shared/components/factories/factory-match-request-dialog/factory-match-dialog.component.ts
@@ -3,16 +3,17 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-factory-match-request-dialog',
-  templateUrl: './match-request-dialog.component.html',
-  styleUrls: ['./match-request-dialog.component.scss']
+  templateUrl: './factory-match-dialog.component.html',
+  styleUrls: ['./factory-match-dialog.component.scss']
 })
-export class MatchRequestDialogComponent implements OnInit {
+export class FactoryMatchDialogComponent implements OnInit {
 
   companyName: string;
   companyId: string;
+  cancelContract = false;
 
   constructor(
-    private dialogRef: MatDialogRef<MatchRequestDialogComponent>,
+    private dialogRef: MatDialogRef<FactoryMatchDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
   }
@@ -20,6 +21,7 @@ export class MatchRequestDialogComponent implements OnInit {
   ngOnInit(): void {
     this.companyName = this.data.companyName;
     this.companyId = this.data.companyId;
+    this.cancelContract = this.data.cancelContract;
   }
 
   onConfirm() {
