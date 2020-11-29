79aebfe3a41e76a8a34247955439d241967814fb
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-29 11:32:27 +0100

    Add method for request or cancel factory matching.

--- carly-web/src/app/carly-shared/components/factories/factory-details/factory-details.component.ts
+++ carly-web/src/app/carly-shared/components/factories/factory-details/factory-details.component.ts
@@ -3,6 +3,11 @@ import {ActivatedRoute} from '@angular/router';
 import {map} from 'rxjs/operators';
 import {FactoryMatch} from '../../../model/factory-match.model';
 import {TEST_DATA} from '../test-factories-data';
+import {MatchStatus} from '../../../model/match-status.enum';
+import {MatDialog} from '@angular/material/dialog';
+import {FactoryMatchDialogComponent} from '../factory-match-request-dialog/factory-match-dialog.component';
+import {FactoryManagementService} from '../../../resources/factory-management.service';
+import {MessageService} from '../../../services/message.service';
 
 @Component({
   selector: 'app-factory-details',
@@ -11,11 +16,20 @@ import {TEST_DATA} from '../test-factories-data';
 })
 export class FactoryDetailsComponent implements OnInit {
 
+  Matched = MatchStatus.MATCHED;
+  Pending = MatchStatus.PENDING_APPROVAL;
+  NotMatched = MatchStatus.NOT_MATCHED;
+
   factories: FactoryMatch[];
   factory: FactoryMatch;
+  cancelMatch = true;
+  isMatched = false;
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private dialog: MatDialog,
+    private messageService: MessageService,
+    private factoryManagementService: FactoryManagementService
   ) {
   }
 
@@ -27,6 +41,7 @@ export class FactoryDetailsComponent implements OnInit {
     });
 
     this.factory = this.factories.find(factory => factory.company.id === factoryId);
+    this.isMatched = this.factory.matchStatus === MatchStatus.MATCHED;
 
     console.log(this.factory);
   }
@@ -34,5 +49,28 @@ export class FactoryDetailsComponent implements OnInit {
   findFactoryId() {
     return this.activatedRoute.params.pipe(map(params => params.id));
   }
+
+  openRequestDialog(cancel = false) {
+    const dialogRef = this.dialog.open(FactoryMatchDialogComponent, {
+      data: {
+        companyName: this.factory.company.companyName,
+        companyId: this.factory.company.id,
+        cancelContract: cancel
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(id => {
+      if (cancel) {
+        this.factoryManagementService.cancelMatching(id);
+      } else {
+        this.factoryManagementService.requestMatching(id);
+      }
+    }, error => {
+        this.messageService.showMessage('Unexpected error occurred!');
+        console.log(error);
+    });
+
+  }
+
 
 }
