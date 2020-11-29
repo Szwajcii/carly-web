e1fec4d7ad40a935884090d52dd775b87cc00694
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-29 11:33:16 +0100

    Implement components - orders data table and cart data.

---
+++ carly-web/src/app/carly-shared/components/orders/order-data-table/order-data-table.component.ts
@@ -0,0 +1,45 @@
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
+@Component({
+  selector: 'app-order-data-table',
+  templateUrl: './order-data-table.component.html',
+  styleUrls: ['./order-data-table.component.scss']
+})
+export class OrderDataTableComponent implements OnInit, AfterViewInit {
+
+  public datasource = new MatTableDataSource([]);
+  @ViewChild('paginator', {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  @Input() set data(data: any[]) {
+    this.setDatasource(data);
+  }
+
+  public displayedColumns: Array<string> = [
+    'id',
+    'status'
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log('Orders data table');
+  }
+
+  ngAfterViewInit() {
+    this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
+  }
+
+  setDatasource(data: any[]) {
+    this.datasource = new MatTableDataSource<any>(data);
+    this.datasource.data = data;
+    this.datasource.paginator = this.paginator;
+    this.datasource.sort = this.sort;
+  }
+
+}
