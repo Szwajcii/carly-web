1a10d2f9646ae27c8be5cecf00784b9f0dcd9330
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-02 19:08:02 +0100

    Start implementing order basket.

--- carly-web/src/app/carly-shared/components/orders/cart-data/cart-data.component.ts
+++ carly-web/src/app/carly-shared/components/orders/cart-data/cart-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CartElement} from '../../../model/cart-element.model';
 
 @Component({
   selector: 'app-cart-data',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDataComponent implements OnInit {
 
+  @Input() cartElements: CartElement[];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick($event: any) {
+    $event.preventDefault();
+  }
+
+  removeItem() {
+
   }
 
 }
