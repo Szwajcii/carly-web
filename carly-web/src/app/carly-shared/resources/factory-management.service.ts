4f2d78d35a85979296d4b23d0178e58a05bca041
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-12-05 13:33:08 +0100

    Add endpoint to find all brands.

--- carly-web/src/app/carly-shared/resources/factory-management.service.ts
+++ carly-web/src/app/carly-shared/resources/factory-management.service.ts
@@ -5,25 +5,31 @@ import {Observable} from 'rxjs';
 import {FactoryMatch} from '../model/factory-match.model';
 import {CompanyMatchResponse} from '../model/company-match-response.model';
 import {CompanyMatchingRequest} from '../model/company-matching-request.model';
+import {Brand} from '../model/brand.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FactoryManagementService {
 
-  factoryManagementApi = 'api/company-match';
+  factoryMatchApi = 'api/company-match';
+  factoryManagementApi = 'api/factory';
 
   constructor(private http: HttpClient) {
   }
 
+  findAllBrands(): Observable<Brand[]> {
+    return this.http.get<Brand[]>(`${this.factoryManagementApi}/all-brands`);
+  }
+
   // todo: Pass company id to BE
   findFactoryMatching(): Observable<FactoryMatch[]> {
-    return this.http.get<FactoryMatch[]>(`${this.factoryManagementApi}`);
+    return this.http.get<FactoryMatch[]>(`${this.factoryMatchApi}`);
   }
 
   requestMatching(matchRequest: CompanyMatchingRequest): Observable<CompanyMatchResponse> {
     console.log('Request matching for id: ', matchRequest.factoryId);
-    return this.http.post<CompanyMatchResponse>(`${this.factoryManagementApi}/request`, matchRequest);
+    return this.http.post<CompanyMatchResponse>(`${this.factoryMatchApi}/request`, matchRequest);
   }
 
   cancelMatching(matchRequest: CompanyMatchingRequest) {
