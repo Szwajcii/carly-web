7999d34c97b5413bfffdf5b7ff86f1c3f2acd3d0
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2021-01-07 08:44:38 +0100

    Changes in FactoriesDataTable component.

--- carly-web/src/app/carly-shared/components/factories/factories-data-table/factories-data-table.component.ts
+++ carly-web/src/app/carly-shared/components/factories/factories-data-table/factories-data-table.component.ts
@@ -7,7 +7,8 @@ import {Company} from '../../../model/company.model';
 import {FactoryMatchDialogComponent} from '../factory-match-request-dialog/factory-match-dialog.component';
 import {MatchStatus} from '../../../model/match-status.enum';
 import {MessageService} from '../../../services/message.service';
-import {CompanyMatchingRequest} from '../../../model/company-matching-request.model';
+import {FactoryManagementService} from '../../../resources/factory-management.service';
+import {CompanyMatchResponse} from '../../../model/company-match-response.model';
 
 @Component({
   selector: 'app-factories-data-table',
@@ -20,10 +21,12 @@ export class FactoriesDataTableComponent implements OnInit, AfterViewInit {
 
   public filter: string;
   public datasource = new MatTableDataSource([]);
-  public cancelMatch = true;
   @ViewChild('paginator', {static: false}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
-  @Output() matchRequest = new EventEmitter();
+  @Input() contextCompanyId: string;
+  matchedFactories: CompanyMatchResponse[] = [];
+  loading = true;
+  noRecords = false;
 
   @Input() set data(data: any[]) {
     this.setDatasource(data);
@@ -31,17 +34,27 @@ export class FactoriesDataTableComponent implements OnInit, AfterViewInit {
 
   public displayedColumns: Array<string> = [
     'name',
-    'status',
+    // 'status',
     'action'
   ];
 
   constructor(
     private dialog: MatDialog,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private factoryManagementService: FactoryManagementService
   ) {
   }
 
   ngOnInit(): void {
+    this.factoryManagementService.findAllMatchedFactories(this.contextCompanyId)
+      .subscribe(resData => {
+        this.matchedFactories = resData.companyFactoriesResponse;
+        console.log(this.matchedFactories);
+        this.setDatasource(this.matchedFactories);
+      }, error => {
+        this.messageService.showMessage('Unexpected error occurred!');
+        console.log(error);
+      });
   }
 
   ngAfterViewInit() {
@@ -66,22 +79,20 @@ export class FactoriesDataTableComponent implements OnInit, AfterViewInit {
   }
 
 
-  openMatchDialog(factory: Company.Model, isCancel = false) {
+  openMatchDialog(companyMatchRequest: CompanyMatchResponse) {
     const dialogRef = this.dialog.open(FactoryMatchDialogComponent, {
       data: {
-        companyName: factory.companyName,
-        companyId: factory.id,
-        cancelContract: isCancel
+        companyName: companyMatchRequest.factoryName,
+        companyId: companyMatchRequest.factoryId,
+        cancelContract: true,
+        factoryMatchId: companyMatchRequest.matchId
       }
     });
 
     dialogRef.afterClosed().subscribe(id => {
       // This won't emit event when we just close modal
-      if (id !== null) {
-        const companyMatchingRequest = new CompanyMatchingRequest();
-        companyMatchingRequest.factoryId = id;
-        companyMatchingRequest.isCancelRequest = isCancel;
-        this.matchRequest.emit(companyMatchingRequest);
+      if (id !== null && id !== undefined) {
+        this.cancelMatching(id);
       }
     }, error => {
       this.messageService.showMessage('Unexpected error occurred!');
@@ -89,4 +100,14 @@ export class FactoriesDataTableComponent implements OnInit, AfterViewInit {
     });
 
   }
+
+  cancelMatching(matchId: string) {
+    this.factoryManagementService.cancelMatching(matchId)
+      .subscribe(resData => {
+        console.log(resData);
+      }, error => {
+        console.log(error);
+      });
+  }
+
 }
